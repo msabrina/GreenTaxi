6b90e52669b4326776c54e7f4f935ffa01d82100
Joey Pinhas <jpinhas97@yahoo.com>
2016-12-07 14:54:42 -0500

    Lotsa stuff

--- models/location.js
+++ models/location.js
@@ -1,22 +1,22 @@
+let key = "AIzaSyBlF8TNXj_FdB5xrIZBHB9uMk1UGegewrk";
+
 function getOriginLatLong (req, res, next) {
-  const originAddress = EncodeURIComponent(req.query.originAddress);
-  let key = "AIzaSyBlF8TNXj_FdB5xrIZBHB9uMk1UGegewrk";
+  const originAddress = encodeURIComponent(req.query.originAddress);
   fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${originAddress}&key=${key}`)
   .then(r => r.json())
-    .then((originData) => {
-      res.originLat = originData.results.location.lat;
-      res.originLong = originData.results.location.lng;
-      next()
-    })
-    .catch((originError) => {
-      console.log("Origin Error is ", error);
-      next();
-    })
-  .then(getDestinationLatLong())
+  .then((originData) => {
+    res.originLat = originData.results.location.lat;
+    res.originLong = originData.results.location.lng;
+    next()
+  })
+  .catch((originError) => {
+    console.log("Origin Error is ", originError);
+    next(originError);
+  });
 }
 
 function getDestinationLatLong(req, res, next) {
-  const destinationAddress = EncodeURIComponent(req.query.destinationAddress);
+  const destinationAddress = encodeURIComponent(req.query.destinationAddress);
   fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${destinationAddress}&key=${key}`)
     .then(r => r.json())
     .then((desinationData) => {
@@ -26,9 +26,8 @@ function getDestinationLatLong(req, res, next) {
     })
     .catch((destinationError) => {
       console.log("Destination Error is ", destinationError);
-      next();
-    })
-    .then(getDistance())
+      next(destinationError);
+    });
 }
 
 function getDistance(req, res, next){
@@ -40,16 +39,39 @@ function getDistance(req, res, next){
     })
     .catch((distanceError) => {
       console.log("Distance Error is ", distanceError);
-      next();
-    })
+      next(distanceError);
+    });
+}
+
+function getWeatherData(req, res, next){
+  let weatherKey = '849f384cb271e06e'
+  fetch(`http://api.wunderground.com/api/${weatherKey}/conditions/q/NY/New_York.json`)
+  .then(r => r.json())
+  .then((weatherData) => {
+    res.tempData = weatherData.currentObservation.temp_f;
+    res.rainData = weatherData.currentObservation.precip_today_in;
+    next();
+  })
+  .catch(error => next(error));
 }
 
 function prepareResponse(req, res, next) {
-  res.data = [
-    res.originLat,
-    res.originLong,
-    res.destinationLat,
-    res.destinationLong,
-    res.tripDistance
-  ]
+  res.data = {
+    origin_lat: res.originLat,
+    origin_long: res.originLong,
+    dest_lat: res.destinationLat,
+    dest_long: res.destinationLong,
+    distance: res.tripDistance,
+    temperature: res.tempData,
+    rainfall: res.rainData,
+  };
+  next();
 }
+
+module.exports = {
+  getOriginLatLong,
+  getDestinationLatLong,
+  getDistance,
+  getWeatherData,
+  prepareResponse,
+};
