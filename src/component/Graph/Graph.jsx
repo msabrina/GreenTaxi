c85ba89b1f954e235d22b7dbcbf4c7be410ac86f
Joey Pinhas <jpinhas97@yahoo.com>
2016-12-08 11:02:19 -0500

    Graph and app optimizations, as well as server stuff - removes lat lon from prediction

--- src/component/Graph/Graph.jsx
+++ src/component/Graph/Graph.jsx
@@ -1,20 +1,18 @@
 import React from 'react';
 import { BarChart } from 'rd3';
+import styles from './Graph.css';
 
 const Graph = props => {
   return (
-    <div>
+    <div className={styles['barchart']}>
       <BarChart
         data={props.data}
-        width={500}
         height={500}
+        width={750}
         fill={'#234981'}
         title={props.chartTitle}
         yAxisLabel={props.yAxisLabel}
         xAxisLabel={props.xAxisLabel}
-        hoverAnimation={props.hover}
-        handleMouseOver={props.mouseOver}
-        handleMouseLeave={props.mouseLeave}
       />
     </div>
   );
